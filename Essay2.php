c759f75de08aa27ffb4cb617504fbc1af9224321
jespetty <61472132+jespetty@users.noreply.github.com>
2020-03-09 21:28:10 -0400

    Update Essay2.php

--- Essay2.php
+++ Essay2.php
@@ -11,6 +11,15 @@
 <?php include("header.php"); 
 ?>
 
+<div>
+<h1>HGIS/Literary Analysis Essay</h1>
+<h2>Research Question: How does the length of time that characters spend in locations correlate to their actions in the novel? 
+</h2>
+<p>
+    There are several points throughout the novel where illnesses and life events force Victor Frankenstein to stay in certain places for long periods of time. To a critical reader, this may seem pointless. For example, he spends several months gathering courage to create a female monster even after he has agreed to do it. Similarly, when the monster is first created and he runs away, Frankenstein spends a long time in the care of his friend, too sick to leave his apartment. What purpose could this serve in the plot? Over how long a period of time does the novel take place? Does the length of time spent in a certain location correlate to the motivation of the characters, whether positive or negative? I am hoping to examine this through a graphical representation of the periods of time against the geographical location. This may turn into a timeline-type essay rather than mapping, but I want to use maps. 
+</p> 
+
+</div>
 
 <!-- Footer Section -->
 <?php include("footer.php"); 
