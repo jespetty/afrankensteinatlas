51c1e69eebe467a10421dcad46e2c40181b4dcd1
jespetty <61472132+jespetty@users.noreply.github.com>
2020-03-09 21:28:02 -0400

    Update Essay1.php

--- Essay1.php
+++ Essay1.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>A Frankenstein Atlas Home</title>
 <title>Postcolonial Theory Essay</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,6 +11,12 @@
 <?php include("header.php"); 
 ?>
 
+<div>
+<h1>Postcolonial Theory Essay</h1>
+<h2>Monster as Human</h2>
+<p>I plan to explore the idea that the debate in Risam's book <i>New Digital Humanities in Theory, Praxis, and Pedagogy</i> of human versus inhuman in technology can be applied to the monster in Frankenstein. When reading the book, the debate the monster has with himself and others over his human-ness struck me as extremely similar to the debate over the creation of AI and how far is "too far". There are countless pop culture references to times when robots become sentient and things go wrong, which can definitely be argued as interpretations of the fears that started with <i>Frankenstein</i>. To take it further, however, the debate of what "human" really means could be an excellent vehicle through which to analyze the monster's ability to grabble with his own identity and how it shaped his actions.</p> 
+
+</div>
 
 <!-- Footer Section -->
 <?php include("footer.php"); 
